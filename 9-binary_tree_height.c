8b66e5f1b76827e24cab9fab6a70138032f644f5
kennyChukwuebuka <kenkinado@gmail>
2023-11-29 02:03:12 +0100

    betty fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_height - Function that measure height of a b-tree
  * @tree: pointer to root node of tree to measure th height
- * Return: if tree is NULL return (0) 
+ * Return: if tree is NULL return (0)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
